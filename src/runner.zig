45a5c30d84a0dcfef2f0c1407f0c4209b34d9ea8
Filippo Menchini <filo@filo-ws.menchini>
2025-10-23 23:57:06 +0200

    run with reader

--- src/runner.zig
+++ src/runner.zig
@@ -1,22 +1,27 @@
 const std = @import("std");
 
-pub fn prompt(input: *std.Io.Reader, ouput: *std.Io.Writer) !void {
+pub fn prompt(input: *std.Io.Reader, output: *std.Io.Writer) !void {
+    try output.print("zlox - REPL v0.1.0\n", .{});
     while (true) {
-        try ouput.print("> ", .{});
-        try ouput.flush();
-        const line = try input.takeDelimiterExclusive('\n');
-        const should_continue = try run(line);
+        try output.print("> ", .{});
+        try output.flush();
+        const should_continue = try run(input);
         if (!should_continue) break;
     }
 }
 
 pub fn file(path: []const u8) !void {
-    _ = try run(path);
+    _ = path;
+    // _ = try run(path);
 }
 
-fn run(bytes: []const u8) !bool {
+fn run(reader: *std.Io.Reader) !bool {
+    const bytes = try reader.takeDelimiterExclusive('\n');
+    reader.toss(1);
+
     if (bytes.len == 0) return true;
     if (std.mem.eql(u8, bytes, "exit")) return false;
+
     std.debug.print("Running code: {s}\n", .{bytes});
     return true;
 }
